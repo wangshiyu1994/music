ed92d8092893136db4dc73e0c439acc12b687667
wangshiyu1994 <1343631004@qq.com>
2017-04-13 19:27:18 +0800

    add

---
+++ js/rem.js
@@ -0,0 +1,20 @@
+﻿/*
+* @Author: Administrator
+* @Date:   2017-04-11 14:16:16
+* @Last Modified by:   Administrator
+* @Last Modified time: 2017-04-11 14:18:01
+*/
+
+'use strict';
+
+window.onload=function(){
+	var designWidth =750;/*定义设计尺寸8*/
+
+	function fontSize(){
+		var CW = document.documentElement.clientWidth;
+		var size=CW/designWidth*100+"px";
+		document.querySelector("html").style.fontSize=size;
+}
+	fontSize();/*函数运行*/
+	window.onresize= fontSize;
+}
